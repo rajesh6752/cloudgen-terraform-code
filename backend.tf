60e42bab7d989e66c7bdaf6f4aa84bab16369343
Rajesh Shukla <41473204+rajesh6752@users.noreply.github.com>
2026-03-28 23:41:19 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-backend-bucket" # Replace with your actual bucket name
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-state-lock"
-  }
-}
